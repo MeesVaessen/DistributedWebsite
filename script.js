2ec3435b9c247700ea9a63a16c34cd250b2a74f7
Semmyv <145569483+Semmyv@users.noreply.github.com>
2024-06-11 14:06:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -115,6 +115,7 @@ async function fetchSaltFromDatabase(username) {
         return null;
     }
 }
+window.onload = openWebSocket();
 
 document.getElementById('dropbox').addEventListener('click', function(event) {
     event.preventDefault();
@@ -274,44 +275,44 @@ function openWebSocket() {
         }
     });
 
-    socket.onmessage = function (event) {
-        console.log(event.data);
-        try {
-            const message = JSON.parse(event.data);
-            console.log(message);
-            const triedPasswords = message.Tried_Passwords || 0;
-            const elapsedTime = message.Elapsed_Time || 0;
+    // socket.onmessage = function (event) {
+    //     console.log(event.data);
+    //     try {
+    //         const message = JSON.parse(event.data);
+    //         console.log(message);
+    //         const triedPasswords = message.Tried_Passwords || 0;
+    //         const elapsedTime = message.Elapsed_Time || 0;
 
-            const maxAttempts = 916132832;
-            const progressPercent = (triedPasswords / maxAttempts) * 100;
+    //         const maxAttempts = 916132832;
+    //         const progressPercent = (triedPasswords / maxAttempts) * 100;
 
-            const progressBar = document.getElementById('progressBar');
-            const triedPasswordsText = document.getElementById('triedPasswords');
-            const elapsedTimeText = document.getElementById('elapsedTime');
+    //         const progressBar = document.getElementById('progressBar');
+    //         const triedPasswordsText = document.getElementById('triedPasswords');
+    //         const elapsedTimeText = document.getElementById('elapsedTime');
            
-            progressBar.style.width = progressPercent + '%';
-            triedPasswordsText.innerText = `Tried Passwords: ${triedPasswords}`;
-            elapsedTimeText.innerText = `Elapsed Time: ${elapsedTime}s`;
+    //         progressBar.style.width = progressPercent + '%';
+    //         triedPasswordsText.innerText = `Tried Passwords: ${triedPasswords}`;
+    //         elapsedTimeText.innerText = `Elapsed Time: ${elapsedTime}s`;
 
-            if (message.Type === 'Password_Found') {
-                const foundPassword = document.getElementById('foundPassword');
-                foundPassword.value = message.Content;
-                foundPassword.style.display = 'block';
-            }
+    //         if (message.Type === 'Password_Found') {
+    //             const foundPassword = document.getElementById('foundPassword');
+    //             foundPassword.value = message.Content;
+    //             foundPassword.style.display = 'block';
+    //         }
 
-            if (message.Type === 'Connection_Token') {
-                _webSocketToken = message.Content;
-                console.log("Connection Token recieved: ", _webSocketToken);
-            }
+    //         if (message.Type === 'Connection_Token') {
+    //             _webSocketToken = message.Content;
+    //             console.log("Connection Token recieved: ", _webSocketToken);
+    //         }
 
-            if (progressPercent >= 100) {
-                const progressContainer = document.getElementById('progressContainer');
-                progressContainer.style.display = 'none';
-            }
-        } catch (error) {
-            console.error('Error parsing WebSocket message:', error);
-        }
-    };
+    //         if (progressPercent >= 100) {
+    //             const progressContainer = document.getElementById('progressContainer');
+    //             progressContainer.style.display = 'none';
+    //         }
+    //     } catch (error) {
+    //         console.error('Error parsing WebSocket message:', error);
+    //     }
+    // };
 
     socket.addEventListener('error', function(event) {
         console.error('WebSocket error:', event);
